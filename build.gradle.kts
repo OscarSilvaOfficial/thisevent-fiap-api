9d9ff3890858b9934c7fa778b6b6ee2fc34e08c2
OscarSilvaOfficial <oscarkaka222@gmail.com>
2021-08-15 20:37:57 -0300

    update de dependências

--- build.gradle.kts
+++ build.gradle.kts
@@ -22,8 +22,10 @@ dependencies {
 	implementation("com.fasterxml.jackson.module:jackson-module-kotlin")
 	implementation("org.jetbrains.kotlin:kotlin-reflect")
 	implementation("org.jetbrains.kotlin:kotlin-stdlib-jdk8")
+	implementation("org.springframework.boot:spring-boot-starter-security")
 	runtimeOnly("mysql:mysql-connector-java")
 	testImplementation("org.springframework.boot:spring-boot-starter-test")
+	testImplementation("org.springframework.security:spring-security-test")
 }
 
 tasks.withType<KotlinCompile> {
